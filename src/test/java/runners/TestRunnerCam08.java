119552faba9785b94e0a010e1e8cf7164ded72e4
ChatGTHB <cihat.kose@hotmail.com>
2023-04-20 12:49:04 +0300

    A separate TestRunner class was created for each feature.

---
+++ src/test/java/runners/TestRunnerCam08.java
@@ -0,0 +1,14 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/java/featureFiles/Cam08.feature"},
+        glue = {"stepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+
+public class TestRunnerCam08 extends AbstractTestNGCucumberTests {
+
+}
